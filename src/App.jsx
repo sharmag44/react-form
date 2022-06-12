3be3a368bf5541e40d426659a776c81fae354f7e
sharmag44 <sharmagshambhu44@gmail.com>
2022-06-12 18:59:18 +0530

    navbar structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
+import Header from "./components/Header/Header"
 export default function App() {
   return (
     <div className="App">
-      <p>Hello world</p>
+      <Header></Header>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 
-export default header = () => {
-	return <div></div>
+export default function Header() {
+	return <div>
+		<h1 className="Header_name">React</h1>
+		<ul className="" Header_nav>
+			<li>Home</li>
+			<li>Github</li>
+			<li>Tutorial</li>
+			<li>Discussion</li>
+		</ul>
+
+	</div>
 }
